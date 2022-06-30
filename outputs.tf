48362b349e7ede6fa4baa15e867abdd312e0dec7
David Sibley <david.sibley@digital.justice.gov.uk>
2022-06-30 15:51:32 +0100

    outputs working sufficiently for unit tests to make use of them

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
 output "sns_subscription_arn" {
   value = { for k, v in aws_sns_topic_subscription.pagerduty_subscription : k => v.arn }
 }
+
+output "sns_topic_arn" {
+  value = { for k, v in data.aws_sns_topic.alarm_topics : k => v.arn}
+}
